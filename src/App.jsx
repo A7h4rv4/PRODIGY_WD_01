f66b72f47a7fb591587290e6cf5d341abe42b88d
A7h4rv4 <132071926+A7h4rv4@users.noreply.github.com>
2023-12-15 15:02:29 +0530

    connect pages with each others

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Footer from "./components/footer";
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
